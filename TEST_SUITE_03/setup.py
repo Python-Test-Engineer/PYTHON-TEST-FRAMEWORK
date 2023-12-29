912efab3930556ca85b857cf96cef21d6de9a728
Craig West <iwswordpress@gmail.com>
2023-12-29 19:23:34 +0000

    added pytest-bdd to 03

--- TEST_SUITE_03/setup.py
+++
@@ -1,5 +0,0 @@
-"""setup.py to be able to do sibling imports - run spip install -e . in root"""
-
-from setuptools import setup, find_packages
-
-setup(name="iws", version="1.0.0", packages=find_packages())
